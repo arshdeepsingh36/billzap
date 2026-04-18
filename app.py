c48db6049f2724b97a4efd249266829cd47a50c9
Arshdeep singh <137316473+arshdeepsingh36@users.noreply.github.com>
2026-04-18 17:51:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# BillZap - Cloud Billing App v2
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
